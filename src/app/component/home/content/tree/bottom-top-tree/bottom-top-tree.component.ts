2a97ecee81f5ec7fc5f5e555518afd307130f43a
Jet Ding <hjet666@gmail.com>
2021-04-11 01:24:13 -0700

    Bottom bottom-top-tree.component

--- src/app/component/home/content/tree/bottom-top-tree/bottom-top-tree.component.ts
+++ src/app/component/home/content/tree/bottom-top-tree/bottom-top-tree.component.ts
@@ -6,4 +6,58 @@ import { BaseTreeComponent } from '../base-tree.component';
   templateUrl: '../left-right-tree/left-right-tree.component.html',
   styleUrls: ['./bottom-top-tree.component.scss'],
 })
-export class BottomTopTreeComponent extends BaseTreeComponent {}
+export class BottomTopTreeComponent extends BaseTreeComponent {
+  createOption() {
+    if (!this.data) {
+      return;
+    }
+    this.option = {
+      backgroundColor: '#fff',
+      tooltip: {
+        trigger: 'item',
+        triggerOn: 'mousemove',
+      },
+
+      series: [
+        {
+          type: 'tree',
+
+          data: [this.data],
+
+          left: '2%',
+          right: '2%',
+          top: '20%',
+          bottom: '8%',
+
+          symbol: 'emptyCircle',
+
+          orient: 'BT',
+
+          expandAndCollapse: true,
+
+          label: {
+            position: 'bottom',
+            rotate: 90,
+            verticalAlign: 'middle',
+            align: 'right',
+          },
+
+          leaves: {
+            label: {
+              position: 'top',
+              rotate: 90,
+              verticalAlign: 'middle',
+              align: 'left',
+            },
+          },
+
+          emphasis: {
+            focus: 'descendant',
+          },
+
+          animationDurationUpdate: 750,
+        },
+      ],
+    };
+  }
+}
--- src/app/component/home/content/tree/top-bottom-tree/top-bottom-tree.component.ts
+++ src/app/component/home/content/tree/top-bottom-tree/top-bottom-tree.component.ts
@@ -6,4 +6,54 @@ import { BaseTreeComponent } from '../base-tree.component';
   templateUrl: '../left-right-tree/left-right-tree.component.html',
   styleUrls: ['./top-bottom-tree.component.scss'],
 })
-export class TopBottomTreeComponent extends BaseTreeComponent {}
+export class TopBottomTreeComponent extends BaseTreeComponent {
+  createOption() {
+    if (!this.data) {
+      return;
+    }
+    this.option = {
+      backgroundColor: '#fff',
+      tooltip: {
+        trigger: 'item',
+        triggerOn: 'mousemove',
+      },
+      series: [
+        {
+          type: 'tree',
+
+          data: [this.data],
+
+          left: '2%',
+          right: '2%',
+          top: '8%',
+          bottom: '20%',
+
+          symbol: 'emptyCircle',
+
+          orient: 'vertical',
+
+          expandAndCollapse: true,
+
+          label: {
+            position: 'top',
+            rotate: -90,
+            verticalAlign: 'middle',
+            align: 'right',
+            fontSize: 9,
+          },
+
+          leaves: {
+            label: {
+              position: 'bottom',
+              rotate: -90,
+              verticalAlign: 'middle',
+              align: 'left',
+            },
+          },
+
+          animationDurationUpdate: 750,
+        },
+      ],
+    };
+  }
+}
