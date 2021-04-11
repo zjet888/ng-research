122cc49673d6c6bd6705443503e6021c5cf13d2e
Jet Ding <hjet666@gmail.com>
2021-04-11 01:37:08 -0700

    Radial tree

--- src/app/component/home/content/tree/radial-tree/radial-tree.component.ts
+++ src/app/component/home/content/tree/radial-tree/radial-tree.component.ts
@@ -6,4 +6,41 @@ import { BaseTreeComponent } from '../base-tree.component';
   templateUrl: '../left-right-tree/left-right-tree.component.html',
   styleUrls: ['./radial-tree.component.scss'],
 })
-export class RadialTreeComponent extends BaseTreeComponent {}
+export class RadialTreeComponent extends BaseTreeComponent {
+  createOption() {
+    if (!this.data) {
+      return;
+    }
+    this.option = {
+      backgroundColor: '#fff',
+      tooltip: {
+        trigger: 'item',
+        triggerOn: 'mousemove',
+      },
+      series: [
+        {
+          type: 'tree',
+
+          data: [this.data],
+
+          top: '18%',
+          bottom: '14%',
+
+          layout: 'radial',
+
+          symbol: 'emptyCircle',
+
+          symbolSize: 7,
+
+          initialTreeDepth: 3,
+
+          animationDurationUpdate: 750,
+
+          emphasis: {
+            focus: 'descendant',
+          },
+        },
+      ],
+    };
+  }
+}
